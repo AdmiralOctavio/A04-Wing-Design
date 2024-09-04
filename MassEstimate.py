5ee7bf4d43428228b69417ef5943bcd2f5aa0358
Flavio <flaviocicero55@gmail.com>
2024-09-04 15:19:17 +0200

    Update MassEstimate.py

--- MassEstimate.py
+++ MassEstimate.py
@@ -2,9 +2,9 @@ import math
 VcrM = 0.77 #mach
 Vcr = VcrM * 296.32
 fcon = 0.05 #contingency fuel ratio
-Rdiv = 45894 #km, divergence range
+Rdiv = 250 #km, divergence range
 Rnom = 2574 #km, Nominal 
-tE = 4985 #loiter time
+tE = 2700 #loiter time
 hCR = 10668 #m, cruise height
 
 
@@ -16,4 +16,9 @@ LD = 1/2 * math.sqrt( (math.pi * AR * e) / (Cd0) )#Lift drag ratio
 
 Rlost = (1/0.7 * LD * (hCR + Vcr**2 /(2*9.80665))) / 1000  #km, lost range from drag
 
-Req = (Rnom + Rlost)*(1+fcon) + 1.2*Rdiv + tE*Vcr #km, equivalent range
+Req = (Rnom + Rlost)*(1+fcon) + 1.2*Rdiv + (tE*Vcr)/1000 #km, equivalent range
+ef = 44 #MJ/kg
+nj = 0.39
+
+f_MTOW = 1 - math.e **(-Req/(1000*nj*ef*LD/9.80665))
+print(f_MTOW)
