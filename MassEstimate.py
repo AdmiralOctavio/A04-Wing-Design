a398f8c9117f7a61320f9ee530506bb8c3be0004
Flavio <flaviocicero55@gmail.com>
2024-09-09 14:53:02 +0200

    Update MassEstimate.py

--- MassEstimate.py
+++ MassEstimate.py
@@ -1,4 +1,6 @@
 import math
+global VcrM
+global Vcr
 VcrM = 0.77 #mach
 Vcr = VcrM * 296.32 #Cruise speed in m/s
 fcon = 0.05 #contingency fuel ratio
@@ -20,9 +22,15 @@ LD = 1/2 * math.sqrt( (math.pi * AR * e) / (Cd0) )#Lift drag ratio
 Rlost = (1/0.7 * LD * (hCR + Vcr**2 /(2*9.80665))) / 1000  #km, lost range from drag
 
 Req = (Rnom + Rlost)*(1+fcon) + 1.2*Rdiv + (tE*Vcr)/1000 #km, equivalent range
+global ef
 ef = 44 #MJ/kg
 
 nj = Vcr/TSFC/ef #Jet efficiency
+
+def jetEfficiency(B):
+    
+    TSFC = 22 * B**(-.19)
+    return Vcr/TSFC/ef
 
 f_MTOW = 1 - math.e **(-Req/(1000*nj*ef*LD/9.80665)) #Fuel / MTOW ratio
 OE_MTOW = 0.566492308 #OE / MTOW ratio
@@ -30,9 +38,10 @@ p_MTOW = 1 - OE_MTOW - f_MTOW #Payload / MTOW ratio
 MTOW = 6355 / p_MTOW
 fuel = MTOW * f_MTOW #kg, fuel mass 
 structure = MTOW * OE_MTOW #kg, structural mass / operating empty
-print(fuel, structure, MTOW)
+#print(fuel, structure, MTOW)
 
 f_landing = 1 - Rnom/Req * f_MTOW
-print(f_landing)
-print(Req-Rnom)
-print(nj)
+#print(f_landing)
+#print(Req-Rnom)
+#print(nj)
+print(jetEfficiency(10))
