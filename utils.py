c05d142b0f692b841db348c924b5d50ca8b1e9c4
Adrian Robinson <adrianrobinson101@gmail.com>
2024-09-11 16:49:23 +0200

    remove duplicate

--- utils.py
+++ utils.py
@@ -1,8 +1,7 @@
+import math
 from enum import Enum
 
-
-def convert_square_meters_to_square_feet(square_meters):
-    return square_meters * 10.76391
+from isa import get_temperature
 
 
 class FlightConfiguration(Enum):
