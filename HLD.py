2e963e0bc5d5250cbb1c78095bc09c6338af2fb4
Flavio <flaviocicero55@gmail.com>
2024-09-18 14:11:19 +0200

    HLD created

---
+++ HLD.py
@@ -0,0 +1,12 @@
+#Delta Cl (For airfoil!):
+
+Plain = 0.9
+Slotted = 1.3
+Fowler = 1.3 #c' / c
+Double_Slotted = 1.6 #c' / c
+Triple_Slotted = 1.9# c' / c
+
+Fixed_Slot = 0.2
+Leading_edge = 0.3
+Kruger = 0.3
+Slat = 0.4 #c' / c
