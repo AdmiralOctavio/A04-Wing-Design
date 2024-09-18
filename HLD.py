89ce5e33f127af8129a728b559510eca17908c10
Flavio <flaviocicero55@gmail.com>
2024-09-18 16:30:31 +0200

    CL func

--- HLD.py
+++ HLD.py
@@ -1,5 +1,7 @@
-#Delta Cl (For airfoil!):
+import math
+import yaml
 
+#Delta Cl (For airfoil!):
 Plain = 0.9
 Slotted = 1.3
 Fowler = 1.3 #c' / c
@@ -14,3 +16,20 @@ Slat = 0.4 #c' / c
 #For full wing
 CL_takeoff = 1.9
 CL_landing = 2.3
+
+deplLanding = 35 #Degrees
+deplTakeoff = 15 #Degrees
+
+#Global parameters used in other files
+with open("aircraft_parameters.yaml") as file:
+   aircraft_parameters = yaml.safe_load(file)
+AR = aircraft_parameters['aspect_ratio']
+ZL = aircraft_parameters['zero_lift_drag_coefficient_estimation']
+e = ZL['euler_efficiency']
+
+def LiftCoefficient(Slat, Flap, Cl): 
+    #Slat, Flap, Cl
+    ClTot = Slat + Flap + Cl
+    CL = ClTot / (1 + ClTot/(AR * math.pi * e) )
+    return CL
+print(LiftCoefficient(0.4, 0.9, 1))
--- MassEstimate.py
+++ MassEstimate.py
@@ -12,44 +12,6 @@ Mpl = 7200 #kg, design payload mass
 
 B = 9 #Bypass Ratio
 
-<<<<<<< Updated upstream
-TSFC = 22*B**(-.19) #Thrust specific fuel consumption
-
-AR = 7.5 #Aspect ratio duh
-SwSratio = 6 #wet to total area ratio or whatever
-Cd0 = 0.0192 #zero lift drag coef
-e = 0.8280596821832961 #Oswald
-global LD
-LD = 1/2 * math.sqrt( (math.pi * AR * e) / (Cd0) )#Lift drag ratio
-global Rlost
-Rlost = (1/0.7 * LD * (hCR + Vcr**2 /(2*9.80665))) / 1000  #km, lost range from drag
-
-global ef
-ef = 44 #MJ/kg
-global nj
-nj = Vcr/TSFC/ef #Jet efficiency
-
-def main(Rnom, Mpl):
-    Req = (Rnom + Rlost)*(1+fcon) + 1.2*Rdiv + (tE*Vcr)/1000 #km, equivalent range
-    f_MTOW = 1 - math.e **(-Req/(1000*nj*ef*LD/9.80665)) #Fuel / MTOW ratio
-    OE_MTOW = 0.566492308 #OE / MTOW ratio
-    p_MTOW = 1 - OE_MTOW - f_MTOW #Payload / MTOW ratio
-    MTOW = Mpl / p_MTOW
-    fuel = MTOW * f_MTOW #kg, fuel mass 
-    structure = MTOW * OE_MTOW #kg, structural mass / operating empty
-    f_landing = 1 - Rnom/Req * f_MTOW #Landing mass fraction
-    return structure, fuel
-
-def ferry():
-    Rnom = 2963 #km
-    Req = (Rnom + Rlost)*(1+fcon) + 1.2*Rdiv + (tE*Vcr)/1000
-    f_MTOW = 1 - math.e **(-Req/(1000*nj*ef*LD/9.80665))
-    OE_MTOW = 1 - f_MTOW
-    OE = main(2574, 6355)[0]
-    MTOW = OE/OE_MTOW
-    Fuel = MTOW - OE
-    return OE_MTOW, MTOW, Fuel
-=======
 TSFC = 22*B**(-.19)
 
 AR = 7.5
@@ -91,4 +53,3 @@ f_landing = 1 - Rnom/Req * f_MTOW
 #print(jetEfficiency(9))
 #print(TSFC)
 
->>>>>>> Stashed changes
