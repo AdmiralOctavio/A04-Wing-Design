7e3311cc951cebad430d669e3f44dedf0df34d9d
Flavio <flaviocicero55@gmail.com>
2024-09-19 10:20:06 +0200

    Update HLD.py

--- HLD.py
+++ HLD.py
@@ -1,6 +1,7 @@
 import math
 import yaml
 
+#Taken from ADSEE II Slides
 #Delta Cl (For airfoil!):
 Plain = 0.9
 Slotted = 1.3
@@ -30,15 +31,19 @@ e = ZL['euler_efficiency']
 def LiftCoefficient(Slat, Flap, Cl, Sf): 
     #Slat, Flap, Cl
     #Flap dCl is localised to the region where its acting, S'/S fraction has to be included (Sf).
-    ClTot = Slat + (Flap * Sf) + Cl #I need to take into account the fact that the flaps only act on part of the wing, slats are full leading edge.
+    ClTot = Slat + (Flap * Sf) + Cl
+    #Official eq: dCL = 0.9 * dCl * Swf/Sw * cos(HingeAngle)
+    #Interpreted: CL = [ 1 + ClTot/(AR * math.pi * e) ] * ClTot * Swf/Sw * cos(HingeAngle)
+    #0.9 coefficient is probably from 1 + ClTot/(AR * math.pi * e)
+    #What is Swf/Sw and Hinge Angle ??
     return ClTot / (1 + ClTot/(AR * math.pi * e) )
 
 ClVal1 = []
 ClVal2 = []
 for i in range(50, 100, 5):
-    ClVal1.append(LiftCoefficient(Slat, Plain, 1.598, i/100))
-    ClVal2.append(LiftCoefficient(Fixed_Slot, Fowler, 1.598, i/100))
+    #ClVal1.append(LiftCoefficient(Slat, Plain, 1.598, i/100))
+    ClVal2.append(LiftCoefficient(Slat, Double_Slotted, 1.323, i/100))
 
-    print(str(ClVal1[int((i-50)/5)]) + " " + str(i) + "%  | 1")
-    print(str(ClVal2[int((i-50)/5)]) + " " + str(i) + "%  | 2")
-    print("\n")
+    #print(str(ClVal1[int((i-50)/5)]) + " " + str(i) + "%  | 1")
+    print(str(  round(ClVal2[int((i-50)/5)], 3)  ) + " " + str(i) + "%")
+    #print("\n")
