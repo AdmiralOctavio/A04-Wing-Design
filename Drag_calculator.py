0296c8adfabf2a000c499a2c36bf9894f59dfbd6
AlexCostache267 <alex.costache267@gmail.com>
2024-10-16 17:06:09 +0200

    Update Drag_calculator.py

--- Drag_calculator.py
+++ Drag_calculator.py
@@ -8,7 +8,7 @@ def Re(L):
     #print(round(Re*10**(-6),2))
     return Re
 
-def Cf_nose(a,L):
+def CF_nose(a,L):
     dx = 0.0001
     Cf = 0
     ReL = Re(L)
@@ -22,8 +22,8 @@ def Cf_nose(a,L):
 def Cf_cyl(s,L):
     ReL = Re(L)
     r = s/L
-    print(ReL)
-    print(r)
+    #print(ReL)
+    #print(r)
     Cf = 0.074/ReL**0.2 * ((1+r)**0.8-r**0.8)
     return Cf
 
@@ -38,7 +38,7 @@ def Cf_plate_lam(L):
     Cf = 1.328/sqrt(ReL)
     return Cf
 
-def Cf_cone(s,L):
+def CF_cone(s,L):
     ReL = Re(L)
     #r = s/L
     #Cf = 0.0592/0.72/ReL**0.2 * ((1+r)**1.8 - r**1.8 - 1.8*r**0.8)
@@ -73,18 +73,11 @@ V_v = [228.3,50.6]
 rho_v = [0.3795,1.225]
 T_v = [218.8,288.15]
 
-case = 1 #0 for cruise, 1 for TO/L
-
-mu = mu_v[case]
-V  = V_v[case]
-rho= rho_v[case]/scaling
-T  = T_v[case]
-gma= 1.4
 R  = 287
 k  = 0.634*10**(-5)
 p_lam_W = 0.1
 p_lam_fus = 0.05
-
+gma= 1.4
 
 
 L1 = 5.23*scaling
@@ -109,49 +102,55 @@ L_eng = 1.9*scaling
 
 
 a = 4*L1/D
-M = V/sqrt(gma*R*T)
 L_tail = sqrt(L3**2 + D**2/4)
 
+for case in range(0,2):#0 for cruise, 1 for TO/L
+    mu = mu_v[case]
+    V  = V_v[case]
+    rho= rho_v[case]/scaling
+    T  = T_v[case]
 
-Cf_nose, s1 = Cf_nose(a,L1) #might be wrong
-s = s1*L1
-Cf_fus = Cf_cyl(s,L2)
-s += L2
-Cf_cone = Cf_cone(s,L_tail) #I'm almost sure it is wrong
+    M = V/sqrt(gma*R*T)
 
-Cf_W = Cf_wing(cr,ct,b)
-Cf_HT = Cf_wing(cr_HT,ct_HT,b_HT)
-Cf_VT = Cf_wing(cr_VT,ct_VT,b_VT*2)/2
+    Cf_nose, s1 = CF_nose(a,L1)
+    s = s1*L1
+    Cf_fus = Cf_cyl(s,L2)
+    s += L2
+    Cf_cone = CF_cone(s,L_tail)
 
-Cf_eng = 2*Cf_cyl(0,L_eng)
+    Cf_W = Cf_wing(cr,ct,b)
+    Cf_HT = Cf_wing(cr_HT,ct_HT,b_HT)
+    Cf_VT = Cf_wing(cr_VT,ct_VT,b_VT*2)/2
+
+    Cf_eng = 2*Cf_cyl(0,L_eng)
 
 
-#S_nose = pi*D**2/6/a**2 * ((1+a)**1.5-1) #this is wrong, check formula
-S_nose = pi*D**2/(6*a**2) * ((1+a**2)**1.5-1)
-S_fus = pi*D*L2
-S_cone = pi*D*L_tail/2
-S_W = S_wing(cr,ct,b)
-S_HT = S_wing(cr_HT,ct_HT,b_HT)
-S_VT = S_wing(cr_VT,ct_VT,b_VT*2)/2
-S_eng = 2*pi*D_eng*L_eng
-S_wet = S_nose + S_fus + S_cone + S_W + S_HT + S_VT + S_eng
+    #S_nose = pi*D**2/6/a**2 * ((1+a)**1.5-1) #this is wrong, check formula
+    S_nose = pi*D**2/(6*a**2) * ((1+a**2)**1.5-1)
+    S_fus = pi*D*L2
+    S_cone = pi*D*L_tail/2
+    S_W = S_wing(cr,ct,b)
+    S_HT = S_wing(cr_HT,ct_HT,b_HT)
+    S_VT = S_wing(cr_VT,ct_VT,b_VT*2)/2
+    S_eng = 2*pi*D_eng*L_eng
+    S_wet = S_nose + S_fus + S_cone + S_W + S_HT + S_VT + S_eng
 
-print('\t','Cf\t\t','Swet')
-print('Nose\t',round(Cf_nose,prec1), '\t', round(S_nose,prec2))
-print('Fuselage',round(Cf_fus,prec1), '\t', round(S_fus,prec2))
-print('Cone\t',round(Cf_cone,prec1), '\t', round(S_cone,prec2))
-print('Wing\t',round(Cf_W,prec1), '\t', round(S_W,prec2))
-print('H tail\t',round(Cf_HT,prec1), '\t', round(S_HT,prec2))
-print('V tail\t',round(Cf_VT,prec1), '\t', round(S_VT,prec2))
-print('Engines\t',round(Cf_eng,prec1), '\t', round(S_eng,prec2))
-print('Swet =', round(S_wet,prec2))
+    print('\t','Cf\t\t','Swet')
+    print('Nose\t',round(Cf_nose,prec1), '\t', round(S_nose,prec2))
+    print('Fuselage',round(Cf_fus,prec1), '\t', round(S_fus,prec2))
+    print('Cone\t',round(Cf_cone,prec1), '\t', round(S_cone,prec2))
+    print('Wing\t',round(Cf_W,prec1), '\t', round(S_W,prec2))
+    print('H tail\t',round(Cf_HT,prec1), '\t', round(S_HT,prec2))
+    print('V tail\t',round(Cf_VT,prec1), '\t', round(S_VT,prec2))
+    print('Engines\t',round(Cf_eng,prec1), '\t', round(S_eng,prec2))
+    print('Swet =', round(S_wet,prec2))
 
-#TEST
-S_ref = (cr+ct)*b/2
-Cf = (S_nose*Cf_nose + S_fus*Cf_fus + S_cone*Cf_cone + S_W*Cf_W + S_HT*Cf_HT + S_VT*Cf_VT + S_eng*Cf_eng)/S_wet
-CD = Cf*S_wet/S_ref
-print('Cf =', round(Cf,prec1))
-print('CD0 =', round(CD,prec1))
+    #TEST
+    S_ref = (cr+ct)*b/2
+    Cf = (S_nose*Cf_nose + S_fus*Cf_fus + S_cone*Cf_cone + S_W*Cf_W + S_HT*Cf_HT + S_VT*Cf_VT + S_eng*Cf_eng)/S_wet
+    CD = Cf*S_wet/S_ref
+    print('Cf =', round(Cf,prec1))
+    print('CD0 =', round(CD,prec1))
 
 
 
