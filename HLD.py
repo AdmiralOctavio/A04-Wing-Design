3b019dde96c94a80648c036b148e5291d7bb4986
Flavio <flaviocicero55@gmail.com>
2024-11-11 17:17:23 +0100

    Update HLD.py

--- HLD.py
+++ HLD.py
@@ -77,6 +77,7 @@ def LiftCoefficient(Slat, Flap, alpha, Cr, Ct):
         '''
         if round(CLValues[ind], 3) >= 2.375 and Alpha_stall < alpha:
             print("Max CL = " + CL + ",  Wing Fraction = " + WF + "%,  Stalling AOA = " + A +  "deg,  AOA = "+ str(alpha) + "deg,  Flap Cr = " + FCHORD + "m,  Flap Ct = " + FCHORD_2 + "m \n")
+            return CL, WF, A, alpha, FCHORD, FCHORD_2, SwfS_TE
             break
 
 LiftCoefficient(Slat, Double_Slotted, 8.2, Cr, Ct)
